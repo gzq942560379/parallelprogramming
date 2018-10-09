4c9695d14bd7fa327e3abb7d539eb271c22bcf43
gzq <942560379@qq.com>
2018-10-09 21:07:39 +0800

    a

---
+++ avx/pi/pi_serial.c
@@ -0,0 +1,27 @@
+//
+// Created by gzq on 18-10-9.
+//
+
+
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../../util/time_utils/time_utils.h"
+
+void main(int argc, char **argv) {
+
+    click_s();
+    size_t dt = 100000000;
+    double pi = 0.0;
+    double delta = 1.0 / dt;
+    double x;
+    for (size_t i = 0; i < dt; i++) {
+        x = i * delta;
+        pi += delta / (1 + x * x);
+    }
+    pi *= 4;
+    click_e();
+    print_milli_interval();
+    printf("pi = %.50lf\n", pi);
+}
